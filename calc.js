42b22cef8fd2d953a02b99f1a3dbc5a4cda3c8cb
velcss <145132538+velcss@users.noreply.github.com>
2023-11-29 07:54:19 +0800

    Add files via upload

--- calc.js
+++ calc.js
@@ -1,4 +1,20 @@
-<script>
-document.write(unescape('function%20myFunction%28%29%20%7B%0A%0Avar%20participants%20%3D%20document.getElementById%28%22n%22%29.value%3B%0Avar%20errors%20%3D%20document.getElementById%28%22e%22%29.value%3B%0A%0Aerrors%20%3D%20errors%20*%20errors%3B%0A%0Aerrors%20%3D%20participants%20*%20errors%3B%0A%0Aerrors%20%3D%20errors%20+%201%3B%0A%0Aparticipants%20%3D%20participants%20/%20errors%3B%0A%0Aparticipants%20%3D%20%20Math.round%28participants%29%3B%0A%0Adocument.getElementById%28%22result%22%29.innerHTML%20%3D%20%22Result%3A%20%22%20+%20participants%3B%20%0A%0A%7D%0A'))
-</script>
+function myFunction() {
+
+var participants = document.getElementById("n").value;
+var errors = document.getElementById("e").value;
+
+errors = errors * errors;
+
+errors = participants * errors;
+
+errors = errors + 1;
+
+participants = participants / errors;
+
+participants =  Math.round(participants);
+
+document.getElementById("result").innerHTML = "Result: " + participants; 
+
+}
+
 
